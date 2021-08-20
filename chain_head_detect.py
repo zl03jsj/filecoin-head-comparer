947b04377d7853dddec5702ef290f0aff10595f3
zl <zl03jsj@163.com>
2021-08-20 18:40:06 +0800

    check api result

--- chain_head_detect.py
+++ chain_head_detect.py
@@ -2,16 +2,21 @@
 import json
 from rpc.conn import _conn, _conns
 from time import sleep
+import logging
 
 with open("./config.json", 'r') as f:
     conn_cfgs = json.load(f)
+    f.close()
 
 conns = _conns()
 
 for c in conn_cfgs:
     conns.insert(_conn(c['name'], c['url'], c['token']))
 
-catch_exeptions = True
+addresses = ["f0128788", "f0127595", "f0123261",
+             "f3qzprefkeragndcicaqgztojarm4pzohn7swwqtmtcx42wykpgxtz6rtpn7xsderun5kigfopv3tydhddx4na",
+             "f3qzprefkeragndcicaqgztojarm4pzohn7swwqtmtcx42wykpgxtz6rtpn7xsderun5kigfopv3tydhddx4na", ]
+
 if __name__ == "__main__":
     print('____________check chain head match with following nodes____________')
     for idx, c in enumerate(conns.conns):
@@ -19,11 +24,18 @@ if __name__ == "__main__":
     print('-------------------------------------------------------------------\n')
 
     while True:
-        if catch_exeptions:
-            try:
-                conns.do_check_heads()
-            except Exception as e:
-                print(e)
-        else:
-            conns.do_check_heads()
+        try:
+            heads, matched = conns.do_check_heads()
+            if matched:
+                tipset = heads[0]
+                # conns.do_check_ChainGetRandomnessFromTickets(tipset)
+                # conns.do_check_CheckChainGetRandomnessFromBeacon(tipset)
+                # conns.do_check_ChainGetBlockMessages(tipset)
+                # conns.do_check_StateMinerSectors(tipset, addresses)
+                # conns.do_check_StateMinerSectorAllocated(tipset, addresses, 900, 1000)
+                # conns.do_check_StateMinerSectorAllocated(tipset, addresses, 900000000000,
+                #                                          900000000010)
+                conns.do_check_StateMinerProvingDeadline(tipset, addresses)
+        except Exception as e:
+            logging.exception(e)
         sleep(4)
--- rpc/conn.py
+++ rpc/conn.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from threading import Thread
+import logging
 
 
 class _thread(Thread):
@@ -13,7 +14,8 @@ class _thread(Thread):
         try:
             self.result = self.func(*self.args)
         except Exception as e:
-            print(e)
+            print("method:%s, params:%s\n", self.args[0], self.args[1])
+            logging.exception(e)
 
     def get_result(self):
         try:
@@ -36,10 +38,13 @@ class _conn:
 
         self.payload = {"jsonrpc": "2.0", "id": 1, }
 
-    def parse_result(self, res):
-        cids = [x['/'] for x in ((res['Cids'] if 'Cids' in res else res['Key']))]
-        b = res['Blocks'][0]
-        return {'cids': cids, 'height': b["Height"] if "Height" in b else b['height'],
+    def parse_head(self, res):
+        # [x['/'] for x in ((res['Cids'] if 'Cids' in res else res['Key']))]
+        cids = res['Key'] if 'Key' in res else (res['Cids'] if 'Cids' in res else None)
+        blks = res['Blocks'][0]
+        return {'cids': cids,
+                'blocks': res['Blocks'],
+                'height': blks["Height"] if "Height" in blks else blks['height'],
                 'name': self.name}
 
     def post(self, method, params):
@@ -49,7 +54,8 @@ class _conn:
 
         if params and not isinstance(params, list):
             raise ValueError('params required to be list')
-        self.payload["params"] = []
+
+        self.payload["params"] = params
 
         res = requests.request("POST", self.url, headers=self.header,
                                data=json.dumps(self.payload))
@@ -57,7 +63,16 @@ class _conn:
             print("unexpected, post to %s failed, status_code=%d" % (
                 self.name, res.status_code))
             return
-        return self.parse_result(json.loads(res.text)["result"])
+        if method == 'Filecoin.ChainHead':
+            return self.parse_head(json.loads(res.text)["result"])
+        else:
+            json_obj = json.loads(res.text)
+            if 'result' in json_obj:
+                return json_obj['result']
+            else:
+                print("error : method:%s, params:%s, message:%s" % (
+                    method, params, json_obj['error']['message']))
+                return json_obj['error']
 
 
 class _conns:
@@ -87,12 +102,8 @@ class _conns:
             t = _thread(func=val.post, args=(method, params))
             threads.append(t)
             t.start()
-
         for index, t in enumerate(threads):
-            r = t.get_result()
-            if r is not None:
-                ress.append(r)
-
+            ress.append(t.get_result())
         return ress
 
     def do_check_heads(self):
@@ -107,12 +118,86 @@ class _conns:
                 matchs = False
                 break
 
-        if matchs:
-            print('expected, height : %d, 100%%-match\n' % (res[0]['height']))
-            return
+        print('|--ChainHead, height:%d, block:%d, %s' % (
+            res[0]['height'], len(res[0]['cids']),
+            '100-%match' if matchs else 'mis-match'))
 
-        print('unexpected, height: %d mis-match' % (res[0]['height'],))
-        for idx, val in enumerate(res):
-            print("|----%s--------\n|-chain_head: height:%d\n|-blocks:%s" % (
-                val['name'], val['height'], val['cids']))
-        print('|-------------------------------------------------------------\n')
+        if False == matchs:
+            for idx, v in enumerate(res):
+                print("|- %-20s: height:%d, block:%d" % (
+                    v['name'], v['height'], len(v['cids'])))
+
+        print('\n')
+
+        return res, matchs
+
+    def do_check_result(self, tipset, method, params, displayName=None):
+        check_info = {'tipset': tipset, 'method': method, 'params': params}
+        res = self.post(check_info['method'], check_info['params'])
+
+        matchs = True
+
+        d_0 = json.dumps(res[0])
+
+        for idx in range(1, len(res)):
+            d = json.dumps(res[idx])
+            if d_0 != d:
+                matchs = False
+                break
+
+        print('|-- method:%s, height:%d, result:%s\nparams:%s\n' % (
+            displayName if displayName is not None else method,
+            tipset['height'], '100-%match' if matchs else 'mis-match', params))
+        return res, matchs
+
+    def do_check_ChainGetRandomnessFromTickets(self, tipset):
+        addr = "f0128788"
+        params = [tipset['cids'], 0, tipset['height'] - 10, addr]
+        for idx in range(1, 9):
+            params[1] = idx
+            self.do_check_result(tipset, 'Filecoin.ChainGetRandomnessFromTickets', params)
+
+    def do_check_CheckChainGetRandomnessFromBeacon(self, tipset):
+        addr = "f0128788"
+        params = [tipset['cids'], 0, tipset['height'] - 10, addr]
+        for idx in range(1, 9):
+            params[1] = idx
+            self.do_check_result(tipset, 'Filecoin.ChainGetRandomnessFromBeacon', params)
+
+    def do_check_ChainGetBlockMessages(self, tipset):
+        for _, blk in enumerate(tipset['blocks']):
+            self.do_check_result(tipset, "Filecoin.ChainReadObj", [blk['Messages']],
+                                 displayName='BlockMessages')
+            self.do_check_result(tipset, 'Filecoin.ChainReadObj',
+                                 [blk['ParentMessageReceipts']],
+                                 displayName='ParentMessageReceipts')
+
+    def do_check_StateMinerSectors(self, tipset, addresses):
+        for _, miner in enumerate(addresses):
+            self.do_check_result(tipset, "Filecoin.StateMinerRecoveries",
+                                 [miner, tipset['blocks'][0]['Parents']])
+            self.do_check_result(tipset, "Filecoin.StateMinerFaults",
+                                 [miner, tipset['blocks'][0]['Parents']])
+            self.do_check_result(tipset, "Filecoin.StateGetActor",
+                                 [miner, tipset['blocks'][0]['Parents']])
+
+    def do_check_StateMinerSectorAllocated(self, tipset, addresses, start, end):
+        for _, miner in enumerate(addresses):
+            for i in range(start, end):
+                self.do_check_result(tipset, "Filecoin.StateMinerSectorAllocated",
+                                     [miner, i, tipset['blocks'][0]['Parents']])
+
+    def do_check_StateMinerProvingDeadline(self, tipset, addresses):
+        for _, miner in enumerate(addresses):
+            res, matches = self.do_check_result(tipset,
+                                                "Filecoin.StateMinerProvingDeadline",
+                                                [miner, tipset['blocks'][0]['Parents']])
+            if matches == True:
+                if not 'Index' in res[0].keys():
+                    for idx, v in enumerate(res):
+                        print('Filecoin.StateMinerProvingDeadline unexpected result, address:%s, key: Index not exist, the result is : %s' % (addresses[idx], v))
+                    return
+
+                self.do_check_result(tipset, "Filecoin.StateMinerPartitions",
+                                     [miner, res[0]['Index'],
+                                      tipset['blocks'][0]['Parents']])
