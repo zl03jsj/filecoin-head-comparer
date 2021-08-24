12dacea1a044ef5527f29f706a821988757ac459
zl <zl03jsj@163.com>
2021-08-24 11:05:17 +0800

    check message estimate gas

--- chain_head_detect.py
+++ chain_head_detect.py
@@ -1,6 +1,6 @@
 #! /usr/bin python3
 import json
-from rpc.conn import _conn, _conns
+from rpc.conn import _conn, _conns_manager
 from time import sleep
 import logging
 
@@ -8,34 +8,56 @@ with open("./config.json", 'r') as f:
     conn_cfgs = json.load(f)
     f.close()
 
-conns = _conns()
+conn_manager = _conns_manager()
 
 for c in conn_cfgs:
-    conns.insert(_conn(c['name'], c['url'], c['token']))
+    if 'enable' in c.keys() and c['enable'] == 'no': continue
+    conn_manager.insert(_conn(c['name'], c['url'], c['token']))
 
-addresses = ["f0128788", "f0127595", "f0123261",
-             "f3qzprefkeragndcicaqgztojarm4pzohn7swwqtmtcx42wykpgxtz6rtpn7xsderun5kigfopv3tydhddx4na",
-             "f3qzprefkeragndcicaqgztojarm4pzohn7swwqtmtcx42wykpgxtz6rtpn7xsderun5kigfopv3tydhddx4na", ]
+miners = ['f02438', "f0128788", "f0127595", "f0123261", "f0135467", "f0142720"]
+accounts = [
+    "f3qzprefkeragndcicaqgztojarm4pzohn7swwqtmtcx42wykpgxtz6rtpn7xsderun5kigfopv3tydhddx4na",
+    "f3sfyonhd3apsolzlpl5uy2a7j7jyktekp7v365l2uqo4chmmf7zmkmsry5qru562yhetnruzflmcnldwow6uq"]
 
-if __name__ == "__main__":
-    print('____________check chain head match with following nodes____________')
-    for idx, c in enumerate(conns.conns):
-        print("%d, %s, %s" % (idx, c.name, c.url))
-    print('-------------------------------------------------------------------\n')
+actors = miners.copy()
+actors.extend(accounts)
 
+print('____________check chain head match with following nodes____________')
+for idx, c in enumerate(conn_manager.conns):
+    print("%d, %s, %s" % (idx, c.name, c.url))
+print('-------------------------------------------------------------------\n')
+
+
+def loop_check_heads():
     while True:
         try:
-            heads, matched = conns.do_check_heads()
-            if matched:
-                tipset = heads[0]
-                # conns.do_check_ChainGetRandomnessFromTickets(tipset)
-                # conns.do_check_CheckChainGetRandomnessFromBeacon(tipset)
-                # conns.do_check_ChainGetBlockMessages(tipset)
-                # conns.do_check_StateMinerSectors(tipset, addresses)
-                # conns.do_check_StateMinerSectorAllocated(tipset, addresses, 900, 1000)
-                # conns.do_check_StateMinerSectorAllocated(tipset, addresses, 900000000000,
-                #                                          900000000010)
-                conns.do_check_StateMinerProvingDeadline(tipset, addresses)
+            conn_manager.do_check_heads()
         except Exception as e:
             logging.exception(e)
         sleep(4)
+
+
+def loop_check_apis():
+    while True:
+        try:
+            heads, matched = conn_manager.do_check_heads()
+            if not matched: continue
+            tipset = heads[0]
+            # conn_manager.do_check_StateGetActor(tipset, miners)
+            conn_manager.do_check_EstimateGas(tipset)
+            # conns.do_check_WalletBalance(tipset, actors)
+            # conns.do_check_ChainGetRandomnessFromTickets(tipset)
+            # conns.do_check_CheckChainGetRandomnessFromBeacon(tipset)
+            # conns.do_check_ChainGetBlockMessages(tipset)
+            # conns.do_check_StateMinerStuff(tipset, tipset, miners)
+            # conns.do_check_StateMinerSectorAllocated(tipset, miners, 900, 1000)
+            # conns.do_check_StateMinerSectorsStuff(tipset, miners)
+            # conns.do_check_StateMinerSectorAllocated(tipset, addresses, 900000000000, 900000000010)
+        except Exception as e:
+            logging.exception(e)
+
+
+sleep(5)
+
+if __name__ == "__main__":
+    loop_check_apis()
--- rpc/conn.py
+++ rpc/conn.py
@@ -2,6 +2,8 @@ import json
 import requests
 from threading import Thread
 import logging
+from functools import reduce
+import operator as op
 
 
 class _thread(Thread):
@@ -60,8 +62,10 @@ class _conn:
         res = requests.request("POST", self.url, headers=self.header,
                                data=json.dumps(self.payload))
         if res.status_code != 200:
-            print("unexpected, post to %s failed, status_code=%d" % (
-                self.name, res.status_code))
+            res_obj = json.loads(res.text)
+            print("unexpected, post to %s failed, status_code=%d, error=%s" % (
+                self.name, res.status_code, res_obj['error']))
+
             return
         if method == 'Filecoin.ChainHead':
             return self.parse_head(json.loads(res.text)["result"])
@@ -70,12 +74,20 @@ class _conn:
             if 'result' in json_obj:
                 return json_obj['result']
             else:
-                print("error : method:%s, params:%s, message:%s" % (
+                print("|- method:%s returns error\n|- params:%s\n|- message:%s" % (
                     method, params, json_obj['error']['message']))
                 return json_obj['error']
 
 
-class _conns:
+def to_josn(d, exclude=[]):
+    obj = d.copy()
+    for path in exclude:
+        path = path.split('/')
+        del reduce(op.getitem, path[:-1], obj)[path[-1]]
+    return json.dumps(obj, default=lambda o: o.__dict__, sort_keys=True)
+
+
+class _conns_manager:
     conns = []
 
     def __init__(self, conns=None):
@@ -86,7 +98,7 @@ class _conns:
             raise ValueError('conns required to be list[conn]')
 
         for c in conns:
-            if not isinstance(c, _conn):
+            if not isinstance(c, _conns_manager):
                 raise ValueError('conns required to be list[conn]')
             self.conns.append(c)
 
@@ -104,6 +116,7 @@ class _conns:
             t.start()
         for index, t in enumerate(threads):
             ress.append(t.get_result())
+        print("\n")
         return ress
 
     def do_check_heads(self):
@@ -124,31 +137,34 @@ class _conns:
 
         if False == matchs:
             for idx, v in enumerate(res):
-                print("|- %-20s: height:%d, block:%d" % (
+                print("|- %+20s: height:%d, block:%d" % (
                     v['name'], v['height'], len(v['cids'])))
-
-        print('\n')
 
         return res, matchs
 
-    def do_check_result(self, tipset, method, params, displayName=None):
+    def do_check_result(self, tipset, method, params, displayName=None, skip=[]):
         check_info = {'tipset': tipset, 'method': method, 'params': params}
         res = self.post(check_info['method'], check_info['params'])
 
         matchs = True
 
-        d_0 = json.dumps(res[0])
+        d_0 = to_josn(res[0], skip)
 
         for idx in range(1, len(res)):
-            d = json.dumps(res[idx])
+            d = to_josn(res[idx], skip)
             if d_0 != d:
                 matchs = False
                 break
 
-        print('|-- method:%s, height:%d, result:%s\nparams:%s\n' % (
+        print('|-- method:%s, height:%d, result:%s\n|-- params:%s' % (
             displayName if displayName is not None else method,
             tipset['height'], '100-%match' if matchs else 'mis-match', params))
-        return res, matchs
+        if not matchs:
+            for idx, res in enumerate(res):
+                print('%d->%s' % (idx, res))
+            print('\n')
+
+        return res[0] if matchs else res, matchs
 
     def do_check_ChainGetRandomnessFromTickets(self, tipset):
         addr = "f0128788"
@@ -172,32 +188,103 @@ class _conns:
                                  [blk['ParentMessageReceipts']],
                                  displayName='ParentMessageReceipts')
 
-    def do_check_StateMinerSectors(self, tipset, addresses):
+    def do_check_StateMinerStuff(self, tipset, addresses):
         for _, miner in enumerate(addresses):
             self.do_check_result(tipset, "Filecoin.StateMinerRecoveries",
                                  [miner, tipset['blocks'][0]['Parents']])
             self.do_check_result(tipset, "Filecoin.StateMinerFaults",
                                  [miner, tipset['blocks'][0]['Parents']])
-            self.do_check_result(tipset, "Filecoin.StateGetActor",
+            self.do_check_result(tipset, "Filecoin.StateMinerInfo",
                                  [miner, tipset['blocks'][0]['Parents']])
+
+    def do_check_StateGetActor(self, tipset, addresses):
+        for _, actor in enumerate(addresses):
+            self.do_check_result(tipset, "Filecoin.StateGetActor",
+                                 [actor, tipset['blocks'][0]['Parents']])
 
     def do_check_StateMinerSectorAllocated(self, tipset, addresses, start, end):
         for _, miner in enumerate(addresses):
             for i in range(start, end):
-                self.do_check_result(tipset, "Filecoin.StateMinerSectorAllocated",
-                                     [miner, i, tipset['blocks'][0]['Parents']])
+                parent_key = tipset['blocks'][0]['Parents']
+                res, matches = self.do_check_result(tipset,
+                                                    "Filecoin.StateMinerSectorAllocated",
+                                                    [miner, i, parent_key])
+                if res == True:
+                    res, matches = self.do_check_result(tipset,
+                                                        'Filecoin.StateSectorGetInfo',
+                                                        [miner, i, parent_key])
 
-    def do_check_StateMinerProvingDeadline(self, tipset, addresses):
+    def do_check_StateMinerSectorsStuff(self, tipset, addresses):
+        parent_key = tipset['blocks'][0]['Parents']
         for _, miner in enumerate(addresses):
-            res, matches = self.do_check_result(tipset,
-                                                "Filecoin.StateMinerProvingDeadline",
-                                                [miner, tipset['blocks'][0]['Parents']])
+            deadlines, matches = self.do_check_result(tipset,
+                                                      "Filecoin.StateMinerProvingDeadline",
+                                                      [miner, parent_key])
             if matches == True:
-                if not 'Index' in res[0].keys():
-                    for idx, v in enumerate(res):
-                        print('Filecoin.StateMinerProvingDeadline unexpected result, address:%s, key: Index not exist, the result is : %s' % (addresses[idx], v))
+                if not 'Index' in deadlines.keys():
+                    for idx, v in enumerate(deadlines):
+                        print(
+                            'error, method:Filecoin.StateMinerProvingDeadline, address:%s, key: Index not exist\nresult : %s' % (
+                                addresses[idx], v))
                     return
 
-                self.do_check_result(tipset, "Filecoin.StateMinerPartitions",
-                                     [miner, res[0]['Index'],
-                                      tipset['blocks'][0]['Parents']])
+                partitions, _ = self.do_check_result(tipset,
+                                                     "Filecoin.StateMinerPartitions",
+                                                     [miner, deadlines['Index'],
+                                                      parent_key])
+                if partitions is None:
+                    continue
+
+                for pt in partitions:
+                    params = [miner, pt['ActiveSectors'], parent_key]
+                    sectors, _ = self.do_check_result(tipset,
+                                                      'Filecoin.StateMinerSectors',
+                                                      params)
+
+    def do_check_WalletBalance(self, tipset, actors):
+        actors = actors.copy()
+        actors.extend(['f01000', 'f1ojyfm5btrqq63zquewexr4hecynvq6yjyk5xv6q',
+                       'f3qfrxne7cg4ml45ufsaxqtul2c33kmlt4glq3b4zvha3msw4imkyi45iyhcpnqxt2iuaikjmmgx2xlr5myuxa'], )
+        for actor in actors:
+            balance, _ = self.do_check_result(tipset,
+                                              'Filecoin.WalletBalance', [actor])
+
+    def load_message_template(self):
+        if hasattr(self, 'message'):
+            return self.message.copy()
+
+        with open("./message.json", 'r') as f:
+            self.message = json.load(f)
+            self.message["GasLimit"] = 0
+            self.message["GasFeeCap"] = "0"
+            self.message["GasPremium"] = "0"
+            f.close()
+            return self.message
+
+    def do_check_EstimateGas(self, tipset):
+        msg = self.load_message_template()
+        actor, matches = self.do_check_result(tipset, 'Filecoin.StateGetActor',
+                                              [msg['From'], tipset['cids']])
+        if not matches:
+            print("|- check StateGetActor mis-match, 'estimategase' won't continue")
+            return
+
+        msg['Nonce'] = actor['Nonce']
+        msg, matches = self.do_check_result(tipset, 'Filecoin.GasEstimateMessageGas',
+                                            [msg, None, tipset['cids']], skip=['CID'])
+        print("|- EstimateMessageGas returns:%s\n" % (msg))
+        return
+
+    def do_check_estimateGasPremium(self):
+        return
+
+    def do_check_esitmateMessageGas(self):
+        return
+
+    def do_check_esitmateFeeCap(self):
+        return
+
+    def do_check_getbaseinfo(self):
+        return
+
+# wallet balance 会出错
